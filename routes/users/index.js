0de6efa3cc9e5c54858f0c5ac93174722f651c7b
Faruq-Hameed <faruqhameed1@gmail.com>
2022-12-14 14:50:28 +0100

    added all users get req, search params and tested with email, name & username

--- routes/users/index.js
+++ routes/users/index.js
@@ -8,19 +8,40 @@ router.get('/', (req, res) => {
     res.status(200).json({'all users': users})
 })
 
-router.get('/search/user/', (req, res) => {
-    const user = users.find(user => user['username'] === req.query.username) 
-    // const user = getByAny(users, 'username', req.query)
-    console.log(user)
-    if (!user) return res.status(404).send(`user with username ${req.query.username} not found`)
-    res.status(200).json({'user': user})
-    
+router.get('/search/user/', (req, res) => { // you can use any params to search for a user
+    let user;
+    if (req.query.username) {//using username to get a particular user 
+        user = getByAny(users, 'username', req.query)
+    }
+    else if (req.query.name) {//using name to get a particular user 
+        user = getByAny(users, 'name', req.query)
+    }
+    else if (req.query.email) {//using email to get a particular user 
+        user = getByAny(users, 'email', req.query)
+    }
+    if (!user ) return res.status(404).send(`user not found`) //for unknown users
+
+    res.status(200).json({ 'user': user })
 })
 
-router.get('/:userId', (req, res) => {
+router.get('/:userId', (req, res) => { 
     const user = getElementById(users, req.params.userId)
     if (!user) return res.status(404).send(`user with id ${req.params.userId} not found`)
     res.status(200).json({'user': user})
+})
+
+router.get('/:userId/friends', (req, res)=>{ //to get all friends of a user with the userId
+    const user = getElementById(users, req.params.userId)// each user has friendsId so we can get the friends with their various id's 
+    if (!user) return res.status(404).send(`user with id ${req.params.userId} not found`)
+    const totalFriends = user.friendsId.length
+    if (totalFriends === 0) return res.status(200).send(`${user.name} has no friends`)
+    let userFriends =  ''
+    for (id of user.friendsId){ //getting the friends with their respective id's  in the user's friends list(friendsId)
+        let friend = getElementById(users, id)
+        userFriends += (user.username + ' is a friend of ' + friend.username + '\n')              
+    }
+    res.status(200).send(`${user.username} has ${totalFriends} friends. They are \n ${userFriends}`)
+    
 })
 
 
